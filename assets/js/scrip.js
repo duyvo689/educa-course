cf67c25eff9a64a3354ea8eebe774b1968821ca0
duyvo689 <vothanhduy689@gmail.com>
2022-06-07 11:27:56 +0700

    js nav active

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -1,7 +1,23 @@
 let menu = document.querySelector('#btn-menu')
 let header = document.querySelector('.header')
 
+
 menu.onclick = function () {
     menu.classList.toggle('fa-times')
     header.classList.toggle('active')
+}
+
+window.onscroll = function () {
+    menu.classList.remove('fa-times')
+    header.classList.remove('active')
+}
+
+var navbar = document.getElementById("navbar-items");
+var btns = navbar.getElementsByClassName("nav");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active1");
+        current[0].className = current[0].className.replace(" active1", "");
+        this.className += " active1";
+    });
 }
